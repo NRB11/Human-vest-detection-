7e50cf5b81afc6ca22d9cfda6a485a236256316b
NicolajRB <34749332+NRB11@users.noreply.github.com>
2024-02-27 13:39:08 +0100

    Update MSG program.py

--- MSG program.py
+++ MSG program.py
@@ -1,22 +1,12 @@
 import socket
-localIP = "127.0.0.1"
-localPort = 20001
+msgFromClient = "Hello UDP Server"
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1", 20001)
 bufferSize = 1024
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
-
-# Create a datagram socket, bind to address and ip
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((localIP, localPort))
-print("UDP server up and listening")
-
-# Listen for incoming datagrams
-while(True):
- bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
- message = bytesAddressPair[0]
- address = bytesAddressPair[1]
- clientMsg = "Message from Client:{}".format(message)
- clientIP = "Client IP Address:{}".format(address)
- print(clientMsg)
- print(clientIP)
- 
+# Create a UDP socket on client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+print(msg)
